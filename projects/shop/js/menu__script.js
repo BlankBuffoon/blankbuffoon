ebbcc76e7d2214394ca2f3cea7739c6cc4470f4d
Alex Terepin <49538696+BlankBuffoon@users.noreply.github.com>
2024-02-13 16:24:16 +0500

    Delete projects directory

--- projects/shop/js/menu__script.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector('.menu__button');
-const nav = document.querySelector('.header__nav');
-
-alert("Javacript подключен");
-
-button.onclick = function() {
-    nav.classList.toggle('open');
-};
